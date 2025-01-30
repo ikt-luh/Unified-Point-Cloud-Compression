d00b21c6e811b9cb69e023ad3160c368c8e08335
Michael Rudolph <michael.rudolph@uni-due.de>
2025-01-30 13:30:42 +0000

    Refactoring model directory is complete

--- model/transforms.py
+++ model/transforms.py
@@ -8,19 +8,18 @@ from model.blocks import *
 
 class AnalysisTransform(nn.Module):
     """
-    Simple Analysis Module consisting of 3 blocks on Non-linear transformations
+    Simple Analysis Module consisting of 3 blocks of non-linear transformations
     """
     def __init__(self, config):
         """
-        Parameters
-        ----------
-        config : dict
-            Dictionary containing information for the transformation
-            keys:
-                C_in: Number of input channels
-                N1: Filters in the 1st layer
-                N2: Filters in the 2nd layer
-                N3: Filters in the 3rd layer
+        Parameters:
+            config (dict):
+                Dictionary containing information for the transformation
+                - C_in: Number of input channels
+                - N1: Filters in the 1st level
+                - N2: Filters in the 2nd level
+                - N3: Filters in the 3rd level
+                - N3: Filters in the bottleneck
         """
         super().__init__()
 
@@ -45,9 +44,19 @@ class AnalysisTransform(nn.Module):
         )
 
 
-
     def count_per_batch(self, x):
-        batch_indices = torch.unique(x.C[:, 0])  # Get unique batch IDs
+        """
+        Utility to count the number of points per batch
+
+        Parameters:
+            x (ME.SparseTensor):
+                Sparse tensor for batch counting
+        
+        Returns:
+            k_per_batch (list):
+                List of the number of points per batch
+        """
+        batch_indices = torch.unique(x.C[:, 0])  
         k_per_batch = []
         for batch_idx in batch_indices:
             k = (x.C[:, 0] == batch_idx).sum().item()
@@ -58,17 +67,17 @@ class AnalysisTransform(nn.Module):
 
     def forward(self, x):
         """
-        Forward pass for the analysis transform
+        Forward pass for the analysis transform.
 
-        Parameters
-        ----------
-        x: ME.SparseTensor
-            Sparse Tensor containing the orignal features
+        Parameters:
+            x (ME.SparseTensor):
+                Sparse Tensor of the point cloud
 
-        returns
-        -------
-        x: ME.SparseTensor
-            Sparse Tensor containing the latent features
+        Returns:
+            y (ME.SparseTensor):
+                Sparse Tensor containing the latent features
+            k (list):
+                List containing the number of points per batch at each level
         """
         k = []
         k.append(self.count_per_batch(x))
@@ -91,20 +100,19 @@ class AnalysisTransform(nn.Module):
 
 class SparseSynthesisTransform(torch.nn.Module):
     """
-    Sparse Decoder/ Synthesis Transform module for Attribute Compression
+    Sparse Decoder/ Synthesis Transform module. 
     Operates by pruning voxels after each upsampling step using the original point cloud geometry.
     """
     def __init__(self, config):
         """
-        Parameters
-        ----------
-        config : dict
-            Dictionary containing information for the transformation
-            keys:
-                C_in: Number of input channels
-                N1: Filters in the 3rd layer
-                N2: Filters in the 2nd layer
-                N3: Filters in the 1st layer
+        Parameters:
+            config (dict):
+                Dictionary containing information for the transformation
+                - C_in: Number of input channels
+                - N1: Filters in the 1rd level
+                - N2: Filters in the 2nd level
+                - N3: Filters in the 3rd level
+                - N4: Filters in the bottleneck
         """
         super().__init__()
 
@@ -114,6 +122,7 @@ class SparseSynthesisTransform(torch.nn.Module):
         N3 = config["N3"]
         N4 = config["N4"]
  
+        # Up-Sampling Blocks
         self.up_1 = nn.Sequential(
             ME.MinkowskiConvolution(in_channels=N4, out_channels=N3, kernel_size=5, stride=1, bias=True, dimension=3),
             MinkowskiGDN(N3, inverse=True),
@@ -127,10 +136,13 @@ class SparseSynthesisTransform(torch.nn.Module):
             MinkowskiGDN(N1, inverse=True),
             ME.MinkowskiGenerativeConvolutionTranspose(in_channels=N1, out_channels=N1//4, kernel_size=5, stride=2, bias=True, dimension=3)
         )
+
+        # Final Color Convolution
         self.color_conv = nn.Sequential(
             ME.MinkowskiConvolution(in_channels=N1//4, out_channels=C_out, kernel_size=1, stride=1, bias=True, dimension=3),
         )
 
+        # Occupancy prediction heads
         self.predict_1 = nn.Sequential(
             ME.MinkowskiConvolution(in_channels=N2, out_channels=N2//2, kernel_size=3, stride=1, bias=True, dimension=3),
             ME.MinkowskiReLU(inplace=False),
@@ -147,101 +159,114 @@ class SparseSynthesisTransform(torch.nn.Module):
             ME.MinkowskiConvolution(in_channels=N4//8, out_channels=1, kernel_size=3, stride=1, bias=True, dimension=3),
         )
 
-
-        self.prune = Pruning()
+        # Pruning
+        self.prune = ME.MinkowskiPruning()
         
         # Auxiliary
         self.down_conv = ME.MinkowskiConvolution(in_channels=1, out_channels=1, kernel_size=3, stride=2, dimension=3)
 
 
 
-    def forward(self, x, coords=None, k=None):
+    def forward(self, y, coords=None, k=None):
         """
-        Forward pass for the synthesis transform
+        Forward pass for the synthesis transform.
 
-        Parameters
-        ----------
-        x: ME.SparseTensor
-            Sparse Tensor containing the latent features
-        coords: ME.SparseTensor
-            Sparse Tensor containing coordinates of the upsampled point cloud
+        Parameters:
+            y (ME.SparseTensor):
+                Sparse Tensor containing the latent features
+            coords (ME.SparseTensor):
+                Sparse Tensor containing coordinates of the upsampled point cloud (optional, for training)
+            k (list):
+                List containing the number of points per level
 
-        returns
-        -------
-        x: ME.SparseTensor
-            Sparse Tensor containing the upsampled features at location of coords
+        Returns:
+            x (ME.SparseTensor):
+                Sparse Tensor containing the upsampled features at location of coords
+            points (list):
+                Optional list of GT-Points for loss computation (training)
+            predictions (list):
+                Optional list of per-level occupancy prediction for loss computation (training)
         """
-        x = self.up_1(x)
+        # 3rd Level
+        x = self.up_1(y)
         predict_1 = self.predict_1(x)
         occupancy_mask = self._topk_prediction(predict_1, k[0])
         coords_1 = x.C[occupancy_mask]
-        x = self.prune(x, coords_1)
+        x = self._prune_tensor(x, coords_1)
 
+        # 2nd Level
         x = self.up_2(x)
         predict_2 = self.predict_2(x)
         occupancy_mask = self._topk_prediction(predict_2, k[1])
         coords_2 = x.C[occupancy_mask]
-        x = self.prune(x, coords_2)
+        x = self._prune_tensor(x, coords_2)
 
+        # 1st Level
         x = self.up_3(x)
         predict_3 = self.predict_3(x)
         occupancy_mask = self._topk_prediction(predict_3, k[2])
         coords_3 = x.C[occupancy_mask]
-        x = self.prune(x, coords_3)
+        x = self._prune_tensor(x, coords_3)
 
+        # Color pred. head
         x = self.color_conv(x)
 
         if coords is not None:
+            # Training
             predictions = [predict_1, predict_2, predict_3]
             
-            # Generate ground truth values
             with torch.no_grad():
                 points_1 = self.down_conv(coords)
                 points_2 = self.down_conv(points_1)
             points = [points_2, points_1, coords]
             return x, points, predictions
         else:
+            # Inference
             return x
 
 
     def _topk_prediction(self, prediction, k):
         """
-        Mask the top-k elements for each batch in prediction to get attributes at predicted points.
-        """
-        batch_indices = torch.unique(prediction.C[:, 0])  # Get unique batch IDs
-        pred_occupancy_mask = torch.zeros_like(prediction.F[:, 0], dtype=torch.bool)
+        Generate a mask for the top-k elements for each batch in prediction to get attributes at predicted points.
 
+        Parameters:
+            prediction (ME.SparseTensor):
+                Sparse Tensor containing the occupancy predictions
+            k (list):
+                List of top-k elements per batch
+        
+        Returns:
+            pred_occupancy_mask (torch.tensor):
+                Mask for the top-k predictions per batch
+        """
+        batch_indices = torch.unique(prediction.C[:, 0])  
+        pred_occupancy_mask = torch.zeros_like(prediction.F[:, 0], dtype=torch.bool)
         for batch_idx in batch_indices:
-            # Mask for current batch
             current_batch_mask = prediction.C[:, 0] == batch_idx
 
-            # Extract the predictions for the current batch and get top-k
             current_preds = prediction.F[current_batch_mask, 0]
             current_k = k[batch_idx]
             _, top_indices = torch.topk(current_preds, int(current_k))
     
-            # Use advanced indexing to set the top-k indices to True
             indices_for_current_batch = torch.nonzero(current_batch_mask).squeeze()
             pred_occupancy_mask[indices_for_current_batch[top_indices]] = True
 
         return pred_occupancy_mask
 
 
-    def _prune_coords(self, x, occupied_points=None):
+    def _prune_tensor(self, x, occupied_points=None):
         """
         Prunes the coordinates after upsampling, only keeping points coinciding with occupied points
 
-        Parameters
-        ----------
-        x: ME.SparseTensor
-            Upsampled point cloud with features
-        occupied_points: ME.SparseTensor
-            Sparse Tensor containing the coordinates to keep
+        Parameters:
+            x (ME.SparseTensor):
+                Upsampled point cloud with features
+            occupied_points (ME.SparseTensor):
+                Sparse Tensor containing the coordinates to keep
 
-        returns
-        -------
-        x: ME.SparseTensor
-            Pruned tensor with features
+        Returns:
+            x (ME.SparseTensor):
+                Pruned tensor with features
         """
         # Define Scaling Factors
         scaling_factors = torch.tensor([1, 1e5, 1e10, 1e15], dtype=torch.int64, device=x.C.device)
